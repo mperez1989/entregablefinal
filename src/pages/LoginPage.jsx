2989e4e8deca5b7946f195762fb09c15356609d4
marco perez <mp4667596@gmail.com>
2023-10-03 16:05:23 -0300

    avance

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form"
 import useAuth from "../hooks/useAuth"
+import "../pages/styles/loginPage.css"
 
 
 
@@ -20,22 +21,22 @@ const LoginPage = () => {
     }
 
   return (
-    <div>
-        <img src="/images/login-img.png" alt="" />
-        <article>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit(submit)}>
-                <div>
+    <div className="login__container">
+        <img className="login__image" src="/images/iniciar-sesion.svg" alt="" />
+        <article className="login">
+            <h1 className="login__title">Login</h1>
+            <form className="login__form" onSubmit={handleSubmit(submit)}>
+                <div className="login__items__container">
                     <label htmlFor="email">Email</label>
                     <input {...register("email")} type="email" id="email" />
                 </div>
-                <div>
+                <div className="login__items__container">
                     <label htmlFor="password">Password</label>
                     <input {...register("password")} type="password" id="password" />
                 </div>
-                <button>Get in</button>
+                <button className="login__container__button">Get in</button>
             </form>
-            <p>Create a new account <Link to="/auth/register">Go to Register</Link></p>
+            <p className="login__text">Create a new account <Link to="/auth/register">Go to Register</Link></p>
         </article>
     </div>
   )
